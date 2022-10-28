e045d0d21acba30ee29814fff4972128588a05c5
rehopi <emekaokechukwu16@gamil.com>
2022-10-28 15:22:03 -0700

    BaseModel File

---
+++ models/base_model.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+    class BaseModel that defines all common
+    attributes/methods for other classes
+"""
+
+
+from uuid import uuid4
+from datetime import datetime
+from models import storage
+
+
+dateformat = "%Y-%m-%dT%H:%M:%S.%f"
+
+
+class BaseModel():
+    """ class BaseModel """
+    def __init__(self, *args, **kwargs):
+        """ Method __init__ initialize the all attibutes """
+        if kwargs:
+            for key, val in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, val)
+                if hasattr(self, 'created_at')\
+                        and type(self.created_at) is str:
+                    self.created_at = datetime.\
+                            strptime(kwargs["updated_at"], dateformat)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
+            storage.new(self)
+
+    def __str__(self):
+        """ __str__ Method """
+        return f"[{BaseModel.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """ save Method """
+        self.updated_at = datetime.today()
+        storage.save()
+
+    def to_dict(self):
+        """ to_dict Method """
+        n_dict = self.__dict__.copy()
+        if "created_at" in n_dict:
+            n_dict["created_at"] = n_dict["created_at"]\
+                    .strftime(dateformat)
+        if "updated_at" in n_dict:
+            n_dict["updated_at"] = n_dict["updated_at"]\
+                    .strftime(dateformat)
+            n_dict["__class__"] = self.__class__.__name__
+        return n_dict
